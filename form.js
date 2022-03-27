8613063cd525c72e2ef684ac46e7c99ed75d2b2a
fikriansyah-31 <syafanurwalinda23@gmail.com>
2022-03-27 08:22:38 +0700

    per

--- form.js
+++ form.js
@@ -1,28 +1,47 @@
-function submitdata() {
-    let name = document.getElementById("input-name").value;
-    let email = document.getElementById("input-email").value;
-    let phone = document.getElementById("input-phone").value;
-    let subject = document.getElementById("input-subject").value;
-    let message = document.getElementById("input-message").value;
-    if (name == "") {
-      return alert("isikan nama");
-    } else if (email == "") {
-      return alert("isikan email");
-    } else if (phone == "") {
-      return alert("isikan nomor telpon");
-    } else if (subject == "") {
-      return alert("isikan subject");
-    } else if (message == "") {
-      return alert("isikan message");
-    }
-    let email_receiver = "fikri@gmail.com";
-    let a = document.createElement("a");
-    
-    // Kasih Comma %2C
-    // Kasih new line %0D%0A
-    a.href = `mailto:${email_receiver}?subject=${subject}&body=Hello my name ${name}%2C${subject}%2C
-      %0D%0A${message}%0D%0A
-      you can contact me in my email ${email} or you can call me in my number ${phone}`;
-    a.click();
-  }
+function submitData(){
+  let name = document.getElementById('input-name').value
+  let email = document.getElementById('input-email').value
+  let phone = document.getElementById('input-phone').value
+  let subject = document.getElementById('input-subject').value
+  let message = document.getElementById('input-message').value
+
+  console.log(name);
+  console.log(email);
+  console.log(phone);
+  console.log(subject);
+  console.log(message);
+
+  // kondisi pengisian form
+
+  if (name == ''){
+      return alert("Nama wajib diisi")
+  } else if (email == ''){
+      return alert("Email wajib diisi")
+  } else if (phone == ''){
+      return alert("Phone wajib diisi")
+  } else if (subject == ''){
+      return alert("Subject wajib diisi")
+  } else if (message == ''){
+      return alert("Message wajib diisi")
+  } 
+
+  let a = document.createElement('a') //pembuatan tag anchor /a
+
+
+  //     a.href = `mailto:${emailReceiver}?subject=Test Subject&body=Hallo my name` //buat tes
+  a.href = `mailto:${email}?subject=${subject} &body=Hallo my name ${name} ${message} Please call me ${phone}`
   
+
+  a.click() // menjalankan tag ancor / mengklik tag anchor
+
+
+  let dataObject ={
+      name: name,  //properti(name:) variable(name)
+      email: email,
+      phone: phone,
+      subject: subject,
+      message: message
+  }
+  console.log(dataObject);
+  window.location.reload();
+}
